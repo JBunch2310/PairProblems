ed2238d4168e2ecb1bb0176e288331210df5339e
Jamie Bunch <JamieBunch@ymail.com>
2022-01-21 15:09:13 -0500

    update

--- Day5Problem2.js
+++ Day5Problem2.js
@@ -1,6 +1,6 @@
 const tempC = 8.5;
 function cToF(temp) {
   const tempF = temp * (9 / 5) + 32;
-  console.log(`${temp} degrees Celsius is equal to ${tempF} Fahrenheit`);
+  console.log(`${temp} degrees Celsius is equal to ${tempF} degrees Fahrenheit`);
 }
 cToF(tempC);
---
+++ Day5Problem3.js
@@ -0,0 +1,11 @@
+const tips = [5, 8, 13, 2, 14, 27, 9, 18]
+
+function calcTips(arr) {
+  for (i = 0; i < arr.length; i++) {
+    let total = 0
+    total += arr[i]
+}
+return total
+  console.log(total)
+}
+calcTips('tips')
